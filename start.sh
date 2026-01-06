af8b885455a17262355538c3b673053299b8cacf
alex <liuhlightning@qq.com>
2026-01-06 18:52:15 +0800

    script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 /root/device \
-	--id 690abd79acac6b29992590e2 \
-	--mqtt-url mqtt://device:device12345@kvvm-ai-mqtt.zcdigitals.com \
-	--ws-url wss://kvvm-ai-serve.zcdigitals.com
+	--serve-url http://192.168.3.45:3000 \
+	--serve-client-id 695229de6d72a035dc9d1b71 \
+	--mqtt-url mqtt://device:device12345@192.168.3.45:1883 \
+	--config-path /root/kvvm-ai.json \
+	--hid-path /dev/hidg1
